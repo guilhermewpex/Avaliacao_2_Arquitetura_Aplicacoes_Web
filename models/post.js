762e889c31945c52e8a96ab9b46b837b3fb22e2f
guilhermewpex <74260646+guilhermewpex@users.noreply.github.com>
2020-12-03 23:05:57 -0300

    Update post.js

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema({
     id : {
         type: String,
-        required: false
+        required: true
     },
     userId: {
         type: String,
